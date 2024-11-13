c440ccfb7d879fb26994db45c4e43b5f5132bac8
Aarnav Tale <aarnavtale@icloud.com>
2024-11-13 17:17:46 -0500

    feat: implement luafile loading from the common areas
    This includes XDG_CONFIG_HOME and XDG_CONFIG_DIRS, falling back to ~/.config/rootbeer if not found.
    Additionally, the luaJIT environment is slightly more limited and sets us up for easier management.

--- include/lua_module.h
+++ include/lua_module.h
@@ -2,9 +2,14 @@
 #define LUA_MODULE_H
 
 #include <lua.h>
-#include <lualib.h>
 #include <lauxlib.h>
 #include <luajit.h>
+#include <lualib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include <string.h>
+#include <assert.h>
 
 // This file contains all of the functions that we can invoke from lua
 // The src/config/functions directory contains implementations for each.
@@ -15,6 +20,7 @@ int rb_lua_debug_test(lua_State *L);
 
 // These functions are all used to help load the module into lua
 
+int rb_lua_create_vm_sandbox(lua_State *L, const char *filename);
 void rb_lua_create_module(lua_State *L);
 
 #endif // LUA_MODULE_H
--- src/main.c
+++ src/main.c
@@ -1,25 +1,13 @@
-#include <lua.h>
-#include <lualib.h>
-#include <lauxlib.h>
-#include <luajit.h>
-
 #include "rootbeer.h"
 #include "lua_module.h"
 
 int main(const int argc, const char *argv[]) {
-	if (argc != 2) {
-		printf("Usage: %s <lua file>\n", argv[0]);
-		return 1;
-	}
-
-	const char *filename = argv[1];
 	lua_State *L = luaL_newstate();
-	luaL_openlibs(L);
-	rb_lua_create_module(L);
-
-	if (luaL_dofile(L, filename) != LUA_OK) {
-		printf("Error: %s\n", lua_tostring(L, -1));
-		lua_pop(L, 1);
+	int status = rb_lua_create_vm_sandbox(L, argv[1]);
+	if (status != 0) {
+		printf("error: could not create vm sandbox\n");
+		lua_close(L);
+		return 1;
 	}
 
 	lua_close(L);
