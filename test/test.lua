887500aad7132e78e2cef43c6f09aa341bec79d4
Aarnav Tale <aarnavtale@icloud.com>
2025-06-13 02:53:23 -0400

    chore: update lua test file

--- test/test.lua
+++ test/test.lua
@@ -21,9 +21,64 @@ end
 require('tale.test2')
 require('test3')
 
-for k, v in pairs(package.preload) do
-	print("preload entry:", k, type(v))
+rb.ref_file('./test.bash')
+
+-- local ok, err = pcall(function()
+-- 	assert(rb.link_file('./test.bash', './test2.bash'))
+-- end)
+
+-- if not ok then
+-- 	print("❌ Test failed:", err)
+-- else
+-- 	print("✅ link_file succeeded")
+-- end
+
+
+-- rb.ref_file('./noexist.bash')
+--
+
+-- Create a random table for lua to json
+local json_data = rb.to_json({
+	test = "foo",
+	bar = "joe"
+})
+
+print("JSON Data: " .. json_data)
+rb.write_file("./test/test.json", json_data)
+
+-- Example of what interpolation can look like
+local bash_conf = {
+	env = {
+		FOO = "bar",
+		BAZ = "qux"
+	},
+
+	aliases = {
+		alias1 = "echo 'This is alias 1'",
+		alias2 = "echo 'This is alias 2'"
+	}
+}
+
+-- Creates a .bashrc
+function create_bash_config(config)
+	local bashrc = ""
+
+	-- Add environment variables
+	if config.env then
+		for key, value in pairs(config.env) do
+			bashrc = bashrc .. "export " .. key .. "=\"" .. value .. "\"\n"
+		end
+	end
+
+	-- Add aliases
+	if config.aliases then
+		for alias, command in pairs(config.aliases) do
+			bashrc = bashrc .. "alias " .. alias .. "=\"" .. command .. "\"\n"
+		end
+	end
+
+	return bashrc
 end
 
-rb.ref_file('./test.bash')
--- rb.ref_file('./noexist.bash')
+local bash_config = rb.interpolate_table(bash_conf, create_bash_config)
+rb.write_file("./test/.bashrc.test", bash_config)
