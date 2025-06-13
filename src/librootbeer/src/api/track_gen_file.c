068b0122c106f655f065101823a688a623098952
Aarnav Tale <aarnavtale@icloud.com>
2025-06-13 02:52:41 -0400

    fix: access on unwritten file should ignore enoent

--- src/librootbeer/src/api/track_gen_file.c
+++ src/librootbeer/src/api/track_gen_file.c
@@ -1,12 +1,16 @@
 #include "rb_rootbeer.h"
+#include <errno.h>
 
 int rb_track_gen_file(rb_lua_t *ctx, const char *path) {
 	if (ctx->ref_filesc >= GENFILES_MAX) {
 		return RB_ULIMIT_GENFILES;
 	}
 
-	if (access(path, F_OK | R_OK) != 0) {
-		return RB_ENOENT;
+	if (access(path, R_OK | W_OK) != 0) {
+		// Check if the errno is EACCES, since we can ignore ENOENT
+		if (errno == EACCES) {
+			return RB_EACCES;
+		}
 	}
 
 	// We need to copy the filename into the context
